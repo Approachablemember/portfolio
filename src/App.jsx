48a5e78e9c74ce2f46d9579d4289b0fd6dcac696
Approachablemember <marmariz01ua@icloud.com>
2023-05-23 22:01:44 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Works from "./components/Works"
 import Contact from "./components/Contact";
 import styled from "styled-components";
 
+
 const Container = styled.div`
   height: 100vh;
   scroll-snap-type: y mandatory;
